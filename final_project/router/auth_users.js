670ed5059f2615cba99f896bc35557b7eb632ba2
Platinait <54156216+Platinait@users.noreply.github.com>
2023-05-18 15:21:21 -0400

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -4,7 +4,6 @@ let books = require("./booksdb.js");
 const regd_users = express.Router();
 
 let users = [];
-
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
 }
@@ -15,14 +14,60 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const {username, password} = req.body;
+
+    // Check if username exists in the users array
+    const user = users.find(u => u.username === username);
+    if (!user) {
+        return res.status(400).json({message: "Invalid username or password"});
+    }
+
+    // Check if password matches
+    if (user.password !== password) {
+        return res.status(400).json({message: "Invalid username or password"});
+    }
+
+    // Generate JWT
+    const token = jwt.sign({username: user.username}, 'secret', {expiresIn: '1h'});
+    res.json({message: "Logged in successfully", token});
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const {isbn} = req.params;
+    const {review} = req.body;
+    const {username} = jwt.verify(req.headers.authorization.split(' ')[1], 'secret');
+
+    // Check if book exists
+    const book = books.find(b => b.isbn === isbn);
+    if (!book) {
+        return res.status(404).json({message: "Book not found"});
+    }
+
+    // Add or update review
+    book.reviews[username] = review;
+    res.json({message: "Review added/updated successfully"});
+});
+
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const {isbn} = req.params;
+    const {username} = jwt.verify(req.headers.authorization.split(' ')[1], 'secret');
+
+    // Check if book exists
+    const book = books.find(b => b.isbn === isbn);
+    if (!book) {
+        return res.status(404).json({message: "Book not found"});
+    }
+
+    // Check if review exists
+    if (!book.reviews[username]) {
+        return res.status(404).json({message: "Review not found"});
+    }
+
+    // Delete review
+    delete book.reviews[username];
+    res.json({message: "Review deleted successfully"});
 });
 
 module.exports.authenticated = regd_users;
